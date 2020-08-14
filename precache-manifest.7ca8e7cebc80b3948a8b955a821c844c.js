a1b13840ed30e3305b2d91cbbdc9c8151f0eae12
Ahmad Jaber <30752881+BionikMlc@users.noreply.github.com>
2020-08-14 22:56:12 +0300

    Updates

---
+++ precache-manifest.7ca8e7cebc80b3948a8b955a821c844c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9f19871fdb29f00a2abbbf7560bb9a",
+    "url": "/foodie/index.html"
+  },
+  {
+    "revision": "325d3780a553d9ac133d",
+    "url": "/foodie/static/css/main.08d8992e.chunk.css"
+  },
+  {
+    "revision": "2e2c9a66965f4521b845",
+    "url": "/foodie/static/js/2.eccb5285.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/foodie/static/js/2.eccb5285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325d3780a553d9ac133d",
+    "url": "/foodie/static/js/main.d905b28b.chunk.js"
+  },
+  {
+    "revision": "f3e583f8f6f500aca603",
+    "url": "/foodie/static/js/runtime-main.b5a0ae38.js"
+  }
+]);
